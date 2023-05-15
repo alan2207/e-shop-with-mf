8af6fa033d8e8dbcb6c8e6c00849041a08e14545
Alan Alickovic <alan2207@live.com>
2023-05-16 00:49:01 +0200

    update

--- apps/marketing/src/components/Promo.vue
+++ apps/marketing/src/components/Promo.vue
@@ -9,17 +9,12 @@
         class="relative flex flex-col items-center max-w-3xl px-6 py-32 mx-auto text-center sm:py-64 lg:px-0"
       >
         <h1 class="text-4xl font-bold tracking-tight text-white lg:text-6xl">
-          New arrivals are here
+          New products are here
         </h1>
-        <p class="mt-4 text-xl text-white">
-          The new arrivals have, well, newly arrived. Check out the latest
-          options from our summer small-batch release while they're still in
-          stock.
-        </p>
         <a
           href="/products"
           class="inline-block px-8 py-3 mt-8 text-base font-medium text-gray-900 bg-white border border-transparent rounded-md hover:bg-gray-100"
-          >Shop New Arrivals</a
+          >Shop New Products</a
         >
       </div>
     </div>
