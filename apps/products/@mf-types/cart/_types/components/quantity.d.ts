40c052a454d8fb1cea1b53a6c45140d9961d902f
Alan Alickovic <alan2207@live.com>
2023-05-17 13:55:47 +0200

    update types

---
+++ apps/products/@mf-types/cart/_types/components/quantity.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+export declare const Quantity: ({ initialValue, onChange, min, max, }: {
+    initialValue?: number | undefined;
+    onChange?: ((value: number) => void) | undefined;
+    min?: number | undefined;
+    max?: number | undefined;
+}) => JSX.Element;
